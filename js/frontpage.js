5752cc2799eda9a49cb763eef8faf76f6aa5b3c5
SePe19 <seba217i@stud.kea.dk>
2022-11-24 10:37:48 +0100

    Frontpage login

---
+++ js/frontpage.js
@@ -0,0 +1,27 @@
+$(".login-btn").click(function () {
+  $(".login-modal").toggle()
+})
+
+const userUrl = "http://localhost:8080/api/auth/signin";
+
+function login() {
+  let username = document.getElementById("login-input-username").value;
+  let password = document.getElementById("login-input-password").value;
+
+  let postLoginRequest = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      "username": username,
+      "password": password,
+    })
+  }
+  console.log("im mister meseeks", postLoginRequest)
+
+  return fetch(userUrl, postLoginRequest)
+    .then(response => response.json())
+    .catch(error => console.log(error));
+}
+
