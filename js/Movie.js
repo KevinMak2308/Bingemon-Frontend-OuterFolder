6d6da3ca64d349c0c77fdf9a12178e673dce06ac
SePe19 <seba217i@stud.kea.dk>
2022-11-28 09:22:47 +0100

    Multifilter filtering by genres, decade and language

--- js/Movie.js
+++ js/Movie.js
@@ -1,17 +1,17 @@
-const url = new URL("http://localhost:8080/api/auth/movies")
+const url = new URL("http://localhost:8080/api/auth/movie-multi-filter")
 const mainMovieDiv = document.getElementById('mainMovieDiv')
 
-const orloriginalLanguage = new URL("http://localhost:8080/api/auth/movieoriginallanguage")
+//const orloriginalLanguage = new URL("http://localhost:8080/api/auth/movieoriginallanguage")
 const originalLanguageSelect = document.getElementById('movieOriginalLanguageChoice')
 
 //Placeholder for a Controller Method that will send a response to the frontend with all the movie genres
 const genreUrl = "http://localhost:8080/api/auth/genres/en"
 const movieGenreDropDownSelect = document.getElementById('genreDropDownList')
 
-
-const languageURL = "http://localhost:8080/api/auth/movielanguage"
+const languageURL = "http://localhost:8080/api/auth/movie-language"
 const languageSelect = document.getElementById('movieLanguageChoice')
 
+let newURLParams = new URLSearchParams(url.search)
 
 function fetchMovieGenres() {
   return fetch(genreUrl)
@@ -20,7 +20,6 @@ function fetchMovieGenres() {
 }
 
 function movieGenreData(data) {
-  console.log("Movie Genre Json Data ", data)
 
   for (let i = 0; i < data.length; i++) {
     const genres = data[i]
@@ -34,16 +33,15 @@ function movieGenreData(data) {
     const selectIndex = movieGenreDropDownSelect.selectedIndex;
     let optionIndex = movieGenreDropDownSelect.options[selectIndex]
     genres.id = optionIndex.value
-    console.log("Movie Genre ID ", genres.id)
 
-    let newURLParams = new URLSearchParams(url.search)
     newURLParams.set('genres', optionIndex.value)
     url.search = newURLParams.toString()
-    console.log("New URL after change ", url)
+    console.log(url.search);
 
     })
   }
 }
+
 
 function fetchMovieLanguage() {
 return fetch(languageURL)
@@ -52,33 +50,30 @@ return fetch(languageURL)
 }
 
 function movieLanguageData(data) {
-  console.log("Language data ", data)
 
   for (let i = 0; i < data.length ; i++) {
-
     let language = data[i]
-    const languageOptions = document.createElement('option')
-    languageOptions.innerText = language.english_name
-    languageOptions.setAttribute('value', language.iso_639_1)
 
+    var languageOption = document.createElement('option')
+    languageOption.innerText = language.english_name
+    languageOption.setAttribute('value', language.iso_639_1)
+    languageSelect.appendChild(languageOption)
 
-    languageSelect.appendChild(languageOptions)
-
-
-    languageSelect.addEventListener("change",(event) => {
-      const selectIndex = languageSelect.selectedIndex;
-      let optionIndex = languageSelect.options[selectIndex]
-      languageOptions.value = optionIndex.value
-
-      let newURLParams = new URLSearchParams(url.search)
-      newURLParams.set('language', optionIndex.value)
-      url.search = newURLParams.toString()
-      console.log("New URL after change ", url)
-
-    })
 
   }
+  languageSelect.addEventListener("change",(event) => {
+    const selectIndex = languageSelect.selectedIndex;
+    let optionIndex = languageSelect.options[selectIndex]
+    languageOption.value = optionIndex.value
+
+    newURLParams.set('language', optionIndex.value)
+    url.search = newURLParams.toString()
+    console.log(languageOption.value);
+
+  })
 }
+
+
 
 
 /*function selectMultipleElements() {
@@ -92,8 +87,19 @@ function movieLanguageData(data) {
     console.log(selected);
   }
 }*/
+document.getElementById('decadeDropDown').onchange = function() {
+  let selected = [];
+  for (let option of document.getElementById('decadeDropDown').options) {
+    if (option.selected) {
+      selected.push(option.value);
+    }
+  }
+  let decade = selected;
+  newURLParams.set("decade", decade.toString());
+  url.search = newURLParams.toString();
+  console.log(url.href);
+}
 
-console.log("Default URL ", url)
 function fetchMoviesForSwipeList() {
   return fetch(url)
     .then(data => data.json())
@@ -101,17 +107,13 @@ function fetchMoviesForSwipeList() {
 }
 
 function movieDataBasedOnCriteria(data) {
-  console.log("Every Movie Json Data", data)
-
       let pageNumbers = data.page
 
       window.onscroll = function(event) {
       if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
       alert("you're at the bottom of the page");
-      let newURLParams = new URLSearchParams(url.search)
       newURLParams.set('page', pageNumbers+1)
       url.search = newURLParams.toString()
-      console.log("NEW URL PAGE NUMBER ", url)
       fetchMoviesForSwipeList()
     }
   }
@@ -121,50 +123,22 @@ function movieDataBasedOnCriteria(data) {
   const movies = data.results[i]
   const movieTable = document.createElement('table');
   const movietextTable = document.createElement('tbody')
-  movieTable.innerText = "Movie Title: " + movies.title;
+  movieTable.innerText = "Movie ID/Title: " + movies.id + "/" + movies.title;
   movietextTable.innerText = "Movie Description: " + movies.overview;
   mainMovieDiv.appendChild(movieTable);
   mainMovieDiv.appendChild(movietextTable);
   }
 }
 
-function fetchOriginalMovieLanguage() {
+/*function fetchOriginalMovieLanguage() {
   return fetch(orloriginalLanguage)
     .then(data => data.json())
     .then(movieOriginalLanguageData)
-}
+}*/
 
-function movieOriginalLanguageData(data) {
-  console.log("Original Language data ", data)
-
-  for (let i = 0; i < data.length ; i++) {
-
-    let orilanguage = data[i]
-    const orilanguageOptions = document.createElement('option')
-    orilanguageOptions.innerText = orilanguage.originalLanguage
-    orilanguageOptions.setAttribute('value', original_language)
-
-
-    originalLanguageSelect.appendChild(orilanguageOptions)
-
-
-    originalLanguageSelect.addEventListener("change",(event) => {
-      const selectIndex = originalLanguageSelect.selectedIndex;
-      let optionIndex = originalLanguageSelect.options[selectIndex]
-      orilanguageOptions.value = optionIndex.value
-
-      let newURLParams = new URLSearchParams(url.search)
-      newURLParams.set('with_original_language', optionIndex.value)
-      url.search = newURLParams.toString()
-      console.log("New URL after change ", url)
-
-    })
-
-  }
-}
 
 fetchMovieLanguage()
 fetchMovieGenres()
-fetchOriginalMovieLanguage()
+//fetchOriginalMovieLanguage()
 
 
