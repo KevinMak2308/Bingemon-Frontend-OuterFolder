df4c00ea9adec1f27c17f1240b98029f3e1df40b
SePe19 <seba217i@stud.kea.dk>
2022-12-04 16:06:07 +0100

    Frontpage and movie styling and content

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,45 +1,6 @@
-// import header fragment // virker ikke nu
-$(function() {
-  $("#navigation").load("header.html");
-});
-
-$(".home-nav").click(function () {
-  $(".sign-up-modal").hide()
-  $(".login-modal").hide()
-})
-
-// open login modal
-$(".login-btn").click(function () {
-  $(".sign-up-modal").hide()
-  $(".login-modal").toggle()
-})
-
-$(".close-login").click(function () {
-  $(".login-modal").toggle()
-})
-
-//open signup modal
-$(".sign-up-btn").click(function () {
-  $(".login-modal").hide()
-  $(".sign-up-modal").toggle()
-})
-
-$(".close-sign-up").click(function () {
-  $(".sign-up-modal").toggle()
-})
-
-//closes login modal and open sign up modal
-
-$(".sign-up-redirect").click(function () {
-  $(".login-modal").toggle()
-  $(".sign-up-modal").toggle()
-})
-
-//closes sign up modal and open login modal
-
-$(".login-redirect").click(function () {
-  $(".login-modal").toggle()
-  $(".sign-up-modal").toggle()
+$(".question-container").click(function() {
+    console.log(this)
+    $(".question-container").toggleClass("open")
 })
 
 const loginUrl = "http://localhost:8080/api/auth/signin";
