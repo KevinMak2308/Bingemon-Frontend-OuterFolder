cbdf6eccb085c5d0f4f8843c7644b663d7d77262
Snimb <72379945+Snimb@users.noreply.github.com>
2022-12-16 11:36:14 +0100

    log out

--- js/header.js
+++ js/header.js
@@ -1,16 +1,39 @@
 const userCookieChecker = document.cookie.split(";").find((row) =>
-    row.startsWith("User="))?.split("=")[1];
+  row.startsWith("User="))?.split("=")[1];
 
 if (!userCookieChecker && !window.location.href === "frontpage.html") {
-    window.location.href = "frontpage.html";
+  window.location.href = "frontpage.html";
 }
 
-if(userCookieChecker) {
-    $(".login-btn").hide()
-    $(".sign-up-btn").hide()
-    $(".swipelist-btn").show()
-    $(".profile-btn").show()
+if (userCookieChecker) {
+  $(".login-btn").hide()
+  $(".sign-up-btn").hide()
+  $(".logout-btn").show()
+  $(".swipelist-btn").show()
+  $(".profile-btn").show()
 }
+
+function deleteAllCookies() {
+  var cookies = document.cookie.split(";");
+  for (var i = 0; i < cookies.length; i++)
+    deleteCookie(cookies[i].split("=")[0]);
+}
+
+function setCookie(name, value, expirydays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (expirydays * 24 * 60 * 60 * 1000));
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = name + "=" + value + "; " + expires;
+}
+
+function deleteCookie(name) {
+  setCookie(name, "", -1);
+}
+
+$(".logout-btn").click(function () {
+  deleteCookie(document.cookie)
+  deleteAllCookies()
+})
 
 // import header fragment // virker ikke nu
 $(".home-nav").click(function () {
@@ -56,13 +79,13 @@ $(".login-redirect").click(function () {
 })
 
 // close modal on click outside at anywhere
-$(document).click(function(event) {
+$(document).click(function (event) {
   if (!$(event.target).closest(".login-form, .login-btn").length) {
     $("body").find(".login-modal").hide();
   }
 });
 
-$(document).click(function(event) {
+$(document).click(function (event) {
   if (!$(event.target).closest(".login-form, .sign-up-btn").length) {
     $("body").find(".sign-up-modal").hide();
   }
